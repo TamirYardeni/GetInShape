9b7ddef8409a3db1b31be5ef4498b46e9e832fff
TAMIR YARDENI <tamir.yardeni1@gmail.com>
2018-01-03 14:42:35 +0200

    Started login/sign in

--- app/src/main/java/tmrapps/getinshapeapp/EntranceActivity.java
+++ app/src/main/java/tmrapps/getinshapeapp/EntranceActivity.java
@@ -1,13 +1,57 @@
 package tmrapps.getinshapeapp;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
-public class EntranceActivity extends Activity {
+public class EntranceActivity extends Activity implements EntranceFragment.OnFragmentInteractionListener, LogFragment.OnFragmentInteractionListener,  SignFragment.OnFragmentInteractionListener {
+
+    private EntranceFragment entranceFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
+        entranceFragment = (EntranceFragment) getFragmentManager().findFragmentById(R.id.main_fragment);
+    }
+
+    @Override
+    public void onEntranceFragmentInteraction(int signOrLog) {
+        if (signOrLog == 1) {
+            showSignFragment();
+
+        } else if (signOrLog == 2) {
+            showLogFragment();
+        }
+    }
+
+    void showSignFragment(){
+        SignFragment frag = SignFragment.newInstance();
+
+        FragmentTransaction tran = getFragmentManager().beginTransaction();
+        tran.hide(entranceFragment);
+
+        tran.add(R.id.main_fragment_container,frag);
+        tran.addToBackStack("tran21");///???????????????????
+        tran.commit();
+    }
+
+    void showLogFragment(){
+        LogFragment frag = LogFragment.newInstance();
+
+        FragmentTransaction tran = getFragmentManager().beginTransaction();
+        tran.hide(entranceFragment);
+
+        tran.add(R.id.main_fragment_container,frag);
+        tran.addToBackStack("tran21");///???????????????????
+        tran.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/tmrapps/getinshapeapp/EntranceFragment.java
@@ -0,0 +1,107 @@
+package tmrapps.getinshapeapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EntranceFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EntranceFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EntranceFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public EntranceFragment() {
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EntranceFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EntranceFragment newInstance(String param1, String param2) {
+        EntranceFragment fragment = new EntranceFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_entrance, container, false);
+        Button signBtn = (Button) view.findViewById(R.id.signBtn);
+        Button logBtn = (Button) view.findViewById(R.id.logBtn);
+
+        signBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mListener != null){
+                    mListener.onEntranceFragmentInteraction(1);
+                }
+            }
+        });
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mListener != null){
+                    mListener.onEntranceFragmentInteraction(2);
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener {
+        void onEntranceFragmentInteraction(int signOrLog);
+    }
+}
